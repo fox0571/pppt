ab03fe6c8c7ca20a641ea75cbf06adb927649049
fox0571 <fox0571@users.noreply.github.com>
2018-08-02 18:48:18 -0500

    update

--- request/admin.py
+++ request/admin.py
@@ -11,10 +11,11 @@ def export_csv(modeladmin, request, queryset):
     writer = csv.writer(response, csv.excel)
     response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
     writer.writerow([
-           smart_str(u"ID"),
-                       smart_str(u"Serial Number"),
+             smart_str(u"ID"),
+             smart_str(u"Serial Number"),
              smart_str(u"Issue"),
              smart_str(u'Detail'),
+             smart_str(u'Note'),
              smart_str(u'tag'),
     ])
     for obj in queryset:
@@ -26,6 +27,7 @@ def export_csv(modeladmin, request, queryset):
              smart_str(obj.serialNumber),
              smart_str(obj.issue),
              smart_str(obj.tsq),
+             smart_str(obj.warrantyNote),
              smart_str(tag),
         ])
     return response
