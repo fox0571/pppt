d03d5908d82d041a442ac704d569a686c4e08fa5
fox0571 <fox0571@users.noreply.github.com>
2018-12-06 11:35:32 -0600

    update

--- units/models.py
+++ units/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+BRANCHES=(
+    ('Brea', 'HQ, Brea, CA'),
+    ('Hayward', 'Hayward, CA'),
+    ('Dallas', 'Dallas, TX'),
+    ('Houston', 'Houston, TX'),
+    ('Washington', 'Kent, WA'),
+    ('Colorado', 'Aurora, CO'),
+    ('Florida', 'Tampa, FL'),
+    ('Illinois', 'Homer Glen, IL'),
+    ('Georgia', 'Doraville, GA'),
+    ('New Jersey', 'Fairfield, NJ'),
+    ('Massachusetts', 'Westborough, MA'),
+    ('Ohio', 'Groveport, OH'),
+)
 class Part(models.Model):
     number=models.CharField(max_length=30)
     name_eng=models.CharField(max_length=90)
@@ -33,3 +47,21 @@ class Unit(models.Model):
 
     class Meta:
         ordering = ('model',)
+
+class PO2China(models.Model):
+    parts = models.ManyToManyField(Part,null=True,blank=True)
+
+    # 0 - created
+    # 1 - ordered
+    # 2 - shipped
+    # 3 - received
+    status = models.IntegerField(default=0)
+    qty = models.IntegerField(default=10)
+    branch = models.CharField(max_length=50,choices=BRANCHES)
+    created_time = models.DateTimeField(auto_now_add=True)
+    ordered_time = models.DateTimeField(null=True,blank=True)
+    shipped_time = models.DateTimeField(null=True,blank=True)
+    received_time = models.DateTimeField(null=True,blank=True)
+
+    def __str__(self):
+        return self.number+self.name_eng
